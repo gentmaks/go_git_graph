cc1fa71e644731446710058495efd5c90641296f
Gent Maksutaj <gent@Gents-MacBook-Air.local>
2025-11-18 01:48:39 -0500

    skeleton for scan and stats

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
-func main() {
-	fmt.Println("Hello World!");
+func stats(email string) {
+	fmt.Println("stats")
+}
+
+func add(folder string) {
+	fmt.Println("scan")
+}
+
+func main(){
+	var folder string
+	var email string
+	flag.StringVar(&folder, "add", "", "Add a new folder to scan for your git repositories")
+	flag.StringVar(&email, "email", "your@email.com", "The email to scan")
+	flag.Parse()
+	if folder != "" {
+		add(folder)
+		return
+	}
+	stats(email)
 }
