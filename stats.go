f29c35d2628a9f956af76b5b4e0ca691a935fb79
Gent Maksutaj <gent.maksutajmax@gmail.com>
2025-11-19 01:44:54 -0500

    generating commit map logic

--- stats.go
+++ stats.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"time"
+	"sort"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing/object"
 )
@@ -69,5 +71,63 @@ func fillCommits (email string, path string, commits map[int]int) map[int]int{
 }
 
 func printCommitsStats(commits map[int]int) {
+	keys := sortMapIntoSlice(commits)
+	cols := buildCols(keys, commits)
+	printCells(cols)
 }
 
+func calcOffset() int {
+    var offset int
+    weekday := time.Now().Weekday()
+
+    switch weekday {
+    case time.Sunday:
+        offset = 7
+    case time.Monday:
+        offset = 6
+    case time.Tuesday:
+        offset = 5
+    case time.Wednesday:
+        offset = 4
+    case time.Thursday:
+        offset = 3
+    case time.Friday:
+        offset = 2
+    case time.Saturday:
+        offset = 1
+    }
+
+    return offset
+}
+
+func getBeginningOfDay(t time.Time) time.Time {
+	year, month, day := t.Date()
+	startOfDay := time.Date(year, month, day, 0, 0, 0, 0, t.Location())
+	return startOfDay
+}
+
+func countDaysSinceDate(date time.Time) int {
+	days := 0
+	now := getBeginningOfDay(time.Now())
+	for date.Before(now) {
+		date = date.Add(time.Hour * 24)
+		days++
+		if days > daysInLastSixMonths {
+			return outOfRange
+		}
+	}
+	return days
+}
+func sortMapIntoSlice(m map[int]int) []int {
+	var keys []int
+	for key, _ := range(m) {
+		keys = append(keys, key)
+	}
+	sort.Ints(keys)
+	return keys
+} 
+func buildCols(keys []int, commits map[int]int) []int {
+	return nil 
+}
+func printCells(cols []int) {
+}
