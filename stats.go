e37784990173ab18bef9f64b1c962c4a4fca6dd5
Gent Maksutaj <gent.maksutajmax@gmail.com>
2025-11-18 23:43:50 -0500

    update on scan impl

--- stats.go
+++ stats.go
@@ -1,17 +1,73 @@
 package main
 
 import (
+	"fmt"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
+const outOfRange = 99999
+const daysInLastSixMonths = 183
+const weeksInLastSixMonths = 26
+
 func stats (email string) {
-	// commits := processRepositories(email)
-	// printCommitsStats(commits)
+	commits := processRepositories(email)
+	printCommitsStats(commits)
 }
 
 func processRepositories(email string) map[int]int {
-	return make(map[int]int)
+	filePath := getDotFilePath()
+	repos := parseFileLinesToSlice(filePath)	
+	daysInMap := daysInLastSixMonths
+
+	commits := make(map[int]int, daysInMap)
+	for i := daysInMap; i > 0; i-- {
+		commits[i] = 0	
+	}
+	
+	for _, path := range(repos) {
+		commits = fillCommits(email, path, commits)
+	}
+	return commits
+}
+
+func fillCommits (email string, path string, commits map[int]int) map[int]int{
+	repo, err := git.PlainOpen(path)
+	if err != nil {
+		panic(err)
+	}
+
+	ref, err := repo.Head()
+	if err != nil {
+		panic(err)
+	}
+
+	iterator, err := repo.Log(&git.LogOptions{From: ref.Hash()})
+	if err != nil {
+		panic(err)
+	}
+
+	offset := calcOffset()
+    err = iterator.ForEach(func(c *object.Commit) error {
+        daysAgo := countDaysSinceDate(c.Author.When) + offset
+
+        if c.Author.Email != email {
+            return nil
+        }
+
+        if daysAgo != outOfRange {
+            commits[daysAgo]++
+        }
+
+        return nil
+    })
+    if err != nil {
+        panic(err)
+    }
+
+    return commits
 }
 
 func printCommitsStats(commits map[int]int) {
-
 }
+
